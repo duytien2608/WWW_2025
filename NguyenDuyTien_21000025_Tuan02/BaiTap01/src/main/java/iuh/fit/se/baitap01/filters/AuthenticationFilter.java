64705bf0e5cfb72529e02b9cc44843bfec7c9794
duytien2608 <125946946+duytien2608@users.noreply.github.com>
2025-10-16 20:18:40 +0700

    Add files via upload

---
+++ NguyenDuyTien_21000025_Tuan02/BaiTap01/src/main/java/iuh/fit/se/baitap01/filters/AuthenticationFilter.java
@@ -0,0 +1,28 @@
+package iuh.fit.se.baitap01.filters;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class AuthenticationFilter extends HttpFilter implements Filter {
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        String usernameFilter = this.getFilterConfig().getInitParameter("username");
+        String passwordFilter = this.getFilterConfig().getInitParameter("password");
+        if (username.equals(usernameFilter) && password.equals(passwordFilter)){
+            chain.doFilter(req, res);
+        }else{
+            PrintWriter writer = res.getWriter();
+            writer.println("Incorrect information entered!");
+            writer.close();
+        }
+    }
+}
